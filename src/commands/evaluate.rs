58b622d3ff888b3545d8e102a203fe6c659590a0
Joydeep Singha <joydeepsingha68@gmail.com>
2026-03-25 13:37:39 +0530

    codecrafters submit [skip ci]

--- src/commands/evaluate.rs
+++ src/commands/evaluate.rs
@@ -3,6 +3,7 @@ use std::fs;
 use crate::commands::parse::parse;
 use crate::commands::tokenize::tokenize;
 use crate::literal::Literal;
+use crate::operator::Operator;
 use crate::parser::expression::Expr;
 
 pub fn run(filename: &str) {
@@ -13,18 +14,46 @@ pub fn run(filename: &str) {
     if !file_contents.is_empty() {
         let tokens = tokenize(&file_contents).0;
         let expr = parse(&mut tokens.into_iter().peekable());
-        println!("{}", evaluate(&expr))
+        match evaluate(&expr) {
+            Literal::Number(n) => {
+                println!("{}", n);
+            }
+            literal => {
+                println!("{}", literal)
+            }
+        };
     }
 }
 
-pub fn evaluate(expr: &Expr) -> String {
+pub fn evaluate(expr: &Expr) -> &Literal {
     match expr {
-        Expr::Literal(literal) => match literal {
-            Literal::Number(n) => format!("{}", n),
-            _ => format!("{}", literal),
-        },
+        Expr::Literal(literal) => literal,
+        // Expr::Literal(literal) => match literal {
+        //     Literal::Number(n) => format!("{}", n),
+        //     _ => format!("{}", literal),
+        // },
         Expr::Grouping(expr) => evaluate(expr),
-        Expr::Unary { prefix, expr } => format!("{}{}", prefix, evaluate(expr)),
+        Expr::Unary { prefix, expr } => match prefix {
+            Operator::Bang => {
+                match **expr {
+                    Expr::Literal(ref literal) => match literal {
+                        Literal::Boolean(false) => &Literal::Boolean(true),
+                        Literal::Nil => &Literal::Boolean(true),
+                        _ => &Literal::Boolean(false),
+                    },
+                    _ => todo!(),
+                }
+                // format!("{}{}", prefix,)
+            }
+            Operator::Minus => match **expr {
+                Expr::Literal(ref literal) => match literal {
+                    Literal::Number(n) => literal,
+                    _ => todo!(),
+                },
+                _ => todo!(),
+            },
+            _ => todo!(),
+        },
         _ => todo!(),
     }
 }
