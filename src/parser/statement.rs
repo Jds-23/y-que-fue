41de3edf1875a5da3a2694470f291ae6c700f073
Joydeep Singha <joydeepsingha68@gmail.com>
2026-03-25 22:05:51 +0530

    codecrafters submit [skip ci]

--- src/parser/statement.rs
+++ src/parser/statement.rs
@@ -5,14 +5,14 @@ use crate::{commands::evaluate::evaluate, parser::expression::Expr};
 #[derive(Debug)]
 pub enum Stmt {
     Print(Expr),
-    Expr(Expr),
+    // Expr(Expr),
 }
 
 impl fmt::Display for Stmt {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Print(expr) => write!(f, "{}", evaluate(&expr)),
-            _ => todo!(),
+            // _ => todo!(),
         }
     }
 }
